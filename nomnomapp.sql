a244199a3c04f554982ea1824f0ca4739d029960
saraabu <pagenoodle@gmail.com>
2025-03-24 17:21:40 -0400

    nomnom fixed

--- nomnomapp.sql
+++ nomnomapp.sql
@@ -1,31 +1,18 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
--- https://www.phpmyadmin.net/
---
 -- Host: localhost
--- Generation Time: Now
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Database: `nomnomapp`
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP DATABASE IF EXISTS `nomnomapp`;
+CREATE DATABASE `nomnomapp`;
+USE `nomnomapp`;
 
---
--- Database: `nomnomapp`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -37,8 +24,16 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-COMMIT;
+-- Table structure for table `saved_recipes`
+DROP TABLE IF EXISTS `saved_recipes`;
+CREATE TABLE `saved_recipes` (
+  `recipe_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `content` TEXT NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`recipe_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
