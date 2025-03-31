0ff05c2f83467f8d763d53edd5797104676b7b8e
Bunroung <142445502+Bunroung@users.noreply.github.com>
2025-03-31 18:09:25 -0400

    Update nomnomapp.sql to add "profilePic" into 'users' table

--- nomnomapp.sql
+++ nomnomapp.sql
@@ -19,6 +19,7 @@ CREATE TABLE `users` (
   `username` VARCHAR(50) NOT NULL,
   `email` VARCHAR(100) NOT NULL UNIQUE,
   `password_hash` VARCHAR(255) NOT NULL,
+  `profilePic` VARCHAR(255),
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`)
